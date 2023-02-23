cc90463c115e9bdd523464e4cc0763975eca5d28
systemsevenfive <ryanwsaunders@icloud.com>
2023-02-22 20:56:46 -0500

    It works!

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,109 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var score = 0
+    @State private var gameOver = false
+    @State private var questionCount = 0
+    @State var countries = ["Estonia",
+                     "France",
+                     "Germany",
+                     "Ireland",
+                     "Italy",
+                     "Nigeria",
+                     "Poland",
+                     "Russia",
+                     "Spain",
+                     "UK",
+                     "US"]
+        .shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            VStack {
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundColor(.white)
+                            .shadow(radius: 5)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                            .foregroundColor(.white)
+                            .shadow(radius: 5)
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                            
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text(scoreMessage)
+        }
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("Continue", action: resetGame)
+        } message: {
+            Text("Final score: \(score)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+            scoreMessage = "Your score is \(score)."
+        } else {
+            score -= 1
+            scoreTitle = "Wrong"
+            scoreMessage = "That is the flag of \(countries[number]). Your score is \(score)."
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        questionCount += 1
+        if questionCount == 3 {
+            gameOver = true
+        } else {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
+    }
+    
+    func resetGame() {
+        score = 0
+        gameOver = false
+        askQuestion()
     }
 }
 
